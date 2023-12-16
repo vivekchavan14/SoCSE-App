368174c9cd170988d6a7bac28d9e7adbfe7821f9
Vivek <vivekchavan382@gmail.com>
2023-12-17 00:33:10 +0530

    Added basic animation to header

--- client/SoCSE/src/components/Header/Header.jsx
+++ client/SoCSE/src/components/Header/Header.jsx
@@ -1,27 +1,30 @@
+// Header.js
+
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../ContextAPI/userContext.jsx';
-import '../Header/Header.css';
+import { motion } from 'framer-motion';
+import './Header.css';
 
 function Header() {
   const [error, setError] = useState(null);
-  const { userInfo, setUserInfo } = useContext(UserContext); 
+  const { userInfo, setUserInfo } = useContext(UserContext);
 
   useEffect(() => {
     fetch('http://localhost:5000/profile', {
       method: 'GET',
       credentials: 'include',
     })
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           return response.json();
         }
         throw new Error('Failed to fetch user info');
       })
-      .then(userInfo => {
-        setUserInfo(userInfo); 
+      .then((userInfo) => {
+        setUserInfo(userInfo);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching user info:', error);
         setError('Failed to fetch user info');
       });
@@ -32,49 +35,80 @@ function Header() {
       method: 'POST',
       credentials: 'include',
     })
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
-          setUserInfo(null); 
+          setUserInfo(null);
         } else {
           throw new Error('Failed to logout');
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error logging out:', error);
         setError('Failed to logout');
       });
   }
 
+  const headerVariants = {
+    initial: { opacity: 0, y: -50 },
+    animate: { opacity: 1, y: 0 },
+  };
+
+  const navItemVariants = {
+    initial: { opacity: 0, y: -50 },
+    animate: { opacity: 1, y: 0, transition: { delay: 0.5 } },
+  };
+
+  const childVariant = {
+    initial: { opacity: 0, y: -20 },
+    animate: { opacity: 1, y: 0 },
+  };
+  
   return (
-    <div className='header-bar'>
-      <header>
+    <motion.div className='header-bar' initial='initial' animate='animate' variants={headerVariants}>
+      <header className='header'>
         <div className='logo'>
           <Link to='/' className='Logo'>
-           <div className='logoSO'><div> <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-awd-mDSSm-S4D50ic2DeXqhh0nES9DtHxwRFlqBtUQ&s"
-              alt="Logo"
-              className="logo-image"
-            /></div>
-            <div> SoCSE-Newsletter </div></div>
+            <div className='logoSO'>
+              <div>
+                {' '}
+                <motion.img
+                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-awd-mDSSm-S4D50ic2DeXqhh0nES9DtHxwRFlqBtUQ&s'
+                  alt='Logo'
+                  className='logo-image'
+                  initial='initial'
+                  animate='animate'
+                  variants={navItemVariants}
+                />
+              </div>
+              <div> SoCSE-Newsletter </div>
+            </div>
           </Link>
         </div>
-        <div className='Navigation'>
-          <nav>
+        <motion.div className='Navigation' initial='initial' animate='animate' variants={headerVariants}>
+        <nav>
             {error || !userInfo ? (
               <>
-                <Link to='/signin'>Login</Link>
-                <Link to='/signup'>Register</Link>
+                <motion.div variants={childVariant}>
+                  <Link to='/signin'>Login</Link>
+                </motion.div>
+                <motion.div variants={childVariant}>
+                  <Link to='/signup'>Register</Link>
+                </motion.div>
               </>
             ) : (
               <>
-                <Link to='/create'>Add Post</Link>
-                <button onClick={logout}>Logout</button>
+                <motion.div variants={childVariant}>
+                  <Link to='/create'>Add Post</Link>
+                </motion.div>
+                <motion.div variants={childVariant}>
+                  <button onClick={logout}>Logout</button>
+                </motion.div>
               </>
             )}
           </nav>
-        </div>
+        </motion.div>
       </header>
-    </div>
+    </motion.div>
   );
 }
 
--- server1/index.js
+++ server1/index.js
@@ -23,7 +23,7 @@ mongoose.connect("mongodb+srv://xyz:xyz@cluster0.svg85z3.mongodb.net/?retryWrite
 
 
 const corsOptions = {
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
